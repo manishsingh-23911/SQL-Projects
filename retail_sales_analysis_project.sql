66d6daf84a700c6036a618bdf87403837e9d79d4
manishsingh-23911 <manishsinghv911@gmail.com>
2025-04-11 01:45:38 +0530

    Add files via upload

---
+++ retail_sales_analysis_project.sql
@@ -0,0 +1,262 @@
+---- CREATING A DATABASE AND IMPORTING DATA ----
+
+-- SQL Retail Sales Analysis 
+CREATE DATABASE sales_analysis_project;
+
+-- Create TABLE and import csv dataset 
+DROP TABLE IF EXISTS retail_sales;
+CREATE TABLE retail_sales
+			(
+				transactions_id	INT PRIMARY KEY,
+				sale_date DATE,
+				sale_time TIME,
+				customer_id INT,
+				gender VARCHAR(15),
+				age INT,
+				category VARCHAR (15),
+				quantity INT,
+				price_per_unit FLOAT,
+				cogs FLOAT,
+				total_sale FLOAT
+			);
+
+-- preview data and check the no. of rows 
+SELECT * FROM retail_sales
+LIMIT 10
+
+SELECT COUNT (*) FROM retail_sales
+-- 2000 rows of data present 
+
+
+
+---- DATA CLEANING ----
+
+
+-- checking for null values 
+SELECT * FROM retail_sales
+WHERE 
+	transactions_id IS NULL
+	OR
+	sale_date IS NULL
+	OR
+	sale_time IS NULL
+	OR
+	customer_id IS NULL 
+	OR
+	gender IS NULL 
+	OR
+	age IS NULL
+	OR
+	category IS NULL
+	OR
+	quantity IS NULL
+	OR
+	price_per_unit IS NULL
+	OR
+	cogs IS NULL
+	OR 
+	total_sale IS NULL
+/* 
+--the following transactions_id have age as null value
+432
+1367
+1391
+1432
+150
+845
+1150
+1845
+797
+921
+--the following transactions_id have quantity, price_per_unit,cogs, total_sale as null value
+679
+746
+1225
+*/
+
+-- Deleting rows which have null values (ignoring null values for age)
+/* Check results before committing
+ ROLLBACK;  -- to undo
+ COMMIT;    -- to confirm
+*/
+
+BEGIN;
+
+DELETE FROM retail_sales
+WHERE 
+	transactions_id IN (679,746,1225)
+
+COMMIT;
+
+
+-- DATA EXPLORATION --
+
+
+-- How many sales do we have?
+
+SELECT COUNT(*) FROM retail_sales
+-- or:  
+SELECT COUNT(transactions_id) FROM retail_sales
+-- We have 1997 sales / sales transactons.
+
+
+-- How many customers do we have?
+
+SELECT COUNT(DISTINCT customer_id) FROM retail_sales
+-- we have 155 unique customers.
+
+
+-- what product categories do we have?
+
+SELECT DISTINCT category FROM retail_sales
+-- we have 3 categories : "Electronics", "Clothing", "Beauty"
+
+
+
+---- DATA ANAlYSIS and answering BUSINESS PROBLEMS ----
+
+
+-- retrieve sales data for all sales made on '2022-11-05'
+
+SELECT * 
+FROM retail_sales 
+WHERE sale_date = '2022-11-05'
+
+-- retrieve all transactions where the category is clothing :
+
+SELECT * 
+FROM retail_sales 
+WHERE category = 'Clothing'
+
+-- Find out the total sales in each category
+
+SELECT SUM(total_sale), category
+FROM retail_sales 
+GROUP BY category
+
+-- retrieve all transaction where category is 'Clothing' and the quantity sold is more than 4 in the month of November 2022
+
+SELECT * FROM retail_sales
+WHERE category = 'Clothing'
+	AND quantity > 3
+	AND sale_date BETWEEN '2022-10-31' AND '2022-12-01' 
+
+
+-- find out the average age of customers who have transactions in 'Beauty' category
+
+SELECT ROUND(AVG(age),2) as average_age, category
+FROM retail_sales
+WHERE category = 'Beauty'
+GROUP BY category 
+-- average age is 40.42 years for this category 
+
+
+-- retrieve all transactions where total_sale is greater than 1000
+
+SELECT * FROM retail_sales
+WHERE total_sale > 1000
+
+-- find out total number of transactions made by each gender in each category 
+
+SELECT gender, category, COUNT(transactions_id) as total_transactions
+FROM retail_sales 
+GROUP BY category, gender
+ORDER BY 2
+
+ 
+
+-- find out the average sales in each month. and also the highest selling month of each year
+-- average sale in each month ->
+SELECT 
+	EXTRACT(YEAR FROM sale_date) as year,
+	EXTRACT(MONTH FROM sale_date) as month,
+	ROUND(AVG(total_sale)::numeric ,2) as avg_sale
+FROM retail_sales
+GROUP BY 1, 2
+ORDER BY 1, 2;
+	
+	
+-- highest selling month in each year ->
+SELECT 
+	year,
+	month,
+	avg_sale
+FROM
+(
+	SELECT 
+		EXTRACT(YEAR FROM sale_date) as year,
+		EXTRACT(MONTH FROM sale_date) as month,
+		ROUND(AVG(total_sale)::numeric ,2) as avg_sale,
+		RANK() OVER(PARTITION BY EXTRACT (YEAR FROM sale_date) ORDER BY AVG(total_sale)DESC) as rank
+	FROM retail_sales
+	GROUP BY 1, 2
+) as t1
+WHERE rank = 1;
+
+
+-- Find the top 5 customers based on highest total sales.
+SELECT 
+	customer_id,
+	SUM(total_sale) as sales_per_customer
+FROM retail_sales
+GROUP BY customer_id
+ORDER BY 2 DESC
+LIMIT 5;
+
+
+-- Fing how many unique customers have made purchased from all 3 categories of products
+
+SELECT COUNT (*)
+FROM
+(
+SELECT customer_id as unique_customers
+FROM retail_sales
+WHERE category IN ('Clothing', 'Beauty', 'Electronics')
+GROUP BY customer_id
+HAVING COUNT(DISTINCT category) = 3
+) as SUB;
+
+-- Find the number of customer who made purchases in each category
+
+SELECT 
+	category,
+	COUNT(DISTINCT customer_id) as no_of_customers
+FROM retail_sales
+GROUP BY category
+ORDER BY no_of_customers
+
+
+-- create 3 shifts(before 12pm, b/w 12pm to 5 pm, after 5 pm) with number of orders per shift
+
+WITH hourly_sale
+AS
+(
+SELECT *,
+	CASE
+		WHEN EXTRACT (HOUR FROM sale_time) < 12 THEN 'Morning'
+		WHEN EXTRACT (HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+	END as shift
+FROM retail_sales
+)
+
+SELECT 
+	shift,
+	COUNT(*) as total_orders
+FROM hourly_sale
+GROUP BY shift
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
